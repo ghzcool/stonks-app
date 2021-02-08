75eea522e7e731da4abf416943b90443e8e95bf9
Pavel Pirogov <pavel.pirogov@wisercat.eu>
2021-02-08 23:37:34 +0200

    - summary column payed

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -9,24 +9,28 @@ function Summary({ onPageChange }) {
   const stocks = useStocks();
   const prices = usePrices();
   const grouped = {};
-  let sum = 0;
+  let payedSum = 0;
+  let gainSum = 0;
   stocks.forEach(item => {
     if (item && prices && prices[item.symbol]) {
       if (!grouped[item.symbol]) {
-        grouped[item.symbol] = 0;
+        grouped[item.symbol] = {payed: 0, gain: 0};
       }
       const price = prices[item.symbol];
-      const value = ((+price.value) * (+item.amount) - (+item.transactionFee || 0)) - ((+item.buyPrice) * (+item.amount) + (+item.transactionFee || 0));
-      sum += value;
-      grouped[item.symbol] += value;
+      const payed = (+item.buyPrice) * (+item.amount) + (+item.transactionFee || 0);
+      const gain = ((+price.value) * (+item.amount) - (+item.transactionFee || 0)) - payed;
+      payedSum += payed;
+      gainSum += gain;
+      grouped[item.symbol].payed += payed;
+      grouped[item.symbol].gain += gain;
     }
   });
   const table = [];
   Object.keys(grouped).forEach(symbol => {
     const price = (prices || {})[symbol] || {};
-    table.push({ symbol, gain: grouped[symbol], currency: price.currency });
+    table.push({ symbol, gain: grouped[symbol].gain, payed: grouped[symbol].payed, currency: price.currency });
   });
-  table.push({ symbol: 'Total', gain: sum, currency: DEFAULT_CURRENCY });
+  table.push({ symbol: 'Total', gain: gainSum, payed: payedSum, currency: DEFAULT_CURRENCY });
 
   return (
     <div className="Summary">
@@ -41,12 +45,14 @@ function Summary({ onPageChange }) {
         <WhiteSpace size="lg"/>
         <Flex>
           <Flex.Item><b>Symbol</b></Flex.Item>
+          <Flex.Item><b>Payed</b></Flex.Item>
           <Flex.Item className={'text-right'}><b>Gain</b></Flex.Item>
         </Flex>
         {table.map(item => <div key={item.symbol}>
           <WhiteSpace size="lg"/>
           <Flex>
             <Flex.Item>{item.symbol}</Flex.Item>
+            <Flex.Item>{(+item.payed).toFixed(2)} {item.currency}</Flex.Item>
             <Flex.Item className={'text-right ' + (item.gain > 0 ? 'gain' : 'lose')}>
               {+item.gain > 0 ? '+' : ''}{(+item.gain).toFixed(2)} {item.currency}
             </Flex.Item>
